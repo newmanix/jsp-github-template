294afe21a2712af671439a033fb0f1f1fbd26c2d
Bill Newman <william.newman@seattlecolleges.edu>
2024-06-03 19:59:38 +0000

    Updates required to make repo run as a repl.

--- replit.nix
+++ replit.nix
@@ -1,8 +1,6 @@
 { pkgs }: {
     deps = [
-        pkgs.graalvm17-ce
+        pkgs.graalvm-ce
         pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
     ];
 }
